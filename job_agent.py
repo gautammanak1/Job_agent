54e3052e0bf59128db48e553efbd31fe5205468b
gautammanak <gautammanak1@gmail.com>
2024-06-06 02:05:31 +0530

    make job finder agent

--- job_agent.py
+++ job_agent.py
@@ -1,1 +1,84 @@
-##
+import http.client
+import json
+from uagents import Agent, Bureau, Context, Model
+from uagents.network import wait_for_tx_to_complete
+from uagents.setup import fund_agent_if_low
+
+class JobDetailsRequest(Model):
+    job_role: str
+    rapidapi_key: str
+
+class JobDetailsResponse(Model):
+    details: str
+
+
+alice = Agent(name="alice", seed="alice secret phrase")
+bob = Agent(name="bob", seed="bob secret phrase")
+
+fund_agent_if_low(bob.wallet.address())
+fund_agent_if_low(alice.wallet.address())
+
+def get_job_details(job_role, rapidapi_key):
+    conn = http.client.HTTPSConnection("indeed11.p.rapidapi.com")
+
+    payload = {
+        "search_terms": job_role,
+        "location": "United States",
+        "page": "1"
+    }
+
+    headers = {
+        'x-rapidapi-key': rapidapi_key,
+        'x-rapidapi-host': "indeed11.p.rapidapi.com",
+        'Content-Type': "application/json"
+    }
+
+    conn.request("POST", "/", json.dumps(payload), headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    return data.decode("utf-8")
+
+
+
+@alice.on_interval(period=30.0)
+async def request_job_details(ctx: Context):
+    job_role = input("Enter the job role: ")
+    rapidapi_key = "be36d48f25msh2cae51229981e29p1d0cc1jsn5e058b8dbb93"  # Fill in with your RapidAPI key
+    details = get_job_details(job_role, rapidapi_key)
+    await ctx.send(
+        bob.address,
+        JobDetailsRequest(
+            job_role=job_role,
+            rapidapi_key=rapidapi_key
+        )
+    )
+    ctx.logger.info(f"Requested job details {job_role}: {details}")
+
+@bob.on_message(model=JobDetailsRequest, replies=JobDetailsResponse)
+async def fetch_job_details(ctx: Context, sender: str, msg: JobDetailsRequest):
+    ctx.logger.info(f"Received job details request from {sender}")
+    details = get_job_details(msg.job_role, msg.rapidapi_key)
+    await ctx.send(
+        alice.address,
+        JobDetailsResponse(details=details)
+    )
+    ctx.logger.info(f"Fetched job details: {details}")
+
+async def confirm_transaction(ctx: Context, sender: str, msg:JobDetailsRequest):
+    ctx.logger.info(f"Received  {sender}: {msg}")
+    tx_resp = await wait_for_tx_to_complete(msg.tx_hash, ctx.ledger)
+    coin_received = tx_resp.events["coin_received"]
+    if (
+        coin_received["receiver"] == str(ctx.wallet.address())
+     
+    ):
+        ctx.logger.info(f"Job Find successful: {coin_received}")
+
+bureau = Bureau()
+bureau.add(alice)
+bureau.add(bob)
+
+if __name__ == "__main__":
+    bureau.run()
